02b49756c410cc8f1510c8d3d168dcaafb20113a
ipascari <jhoonnyc@gmail.com>
2020-11-30 20:42:34 +0200

    add session examples about command-line arguments

--- src/basics/executables/intellij/Demo.java
+++ src/basics/executables/intellij/Demo.java
@@ -2,6 +2,14 @@ package basics.executables.intellij;
 
 public class Demo {
     static public void main(String... arguments){
+        //command line arguments : ["1", "2", "3", "erte" ]
+        //positions                 0     1    2     3
+        //arguments[0] = "1"
+        //arguments[1] = "2"
+        if (arguments[0].equals("dev")) {
+            //compute something long and complex
+            System.out.println("Computing something complex...");
+        }
         System.out.println("Hey, from Intellij");
     }
 }
